dc05b2f65db0ae06e371efe751fe375b8ac959d5
Ankit Rana <ankitrana.95@outlook.com>
2020-07-12 00:58:39 +0530

    Fix corner cases

--- src/com/program/Searching/PeakElement.java
+++ src/com/program/Searching/PeakElement.java
@@ -8,13 +8,13 @@ public class PeakElement {
     private static int findPeakElement(int[] arr, int low, int high){
         int mid = low + (high - low)/2;
         if(mid == 0){
-            if(arr[mid] > arr[mid+1])
+            if(arr[mid] >= arr[mid+1])
                 return arr[mid];
         } else if(mid == arr.length-1){
-            if(arr[mid] > arr[mid-1])
+            if(arr[mid] >= arr[mid-1])
                 return arr[mid];
         }else{
-            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1])
+            if(arr[mid] >= arr[mid-1] && arr[mid] >= arr[mid+1])
                 return arr[mid];
             else
                 if(arr[mid] > arr[mid-1])
